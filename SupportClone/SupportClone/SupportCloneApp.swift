f6bee88d9ce8741c50ec1ad62b6ce9fdfa9c025c
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2023-08-14 14:08:30 -0300

    Criar o projeto no xCode

---
+++ SupportClone/SupportClone/SupportCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupportCloneApp.swift
+//  SupportClone
+//
+//  Created by Gabriel Vicentin Negro on 14/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SupportCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
