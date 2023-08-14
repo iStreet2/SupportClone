829135d25eb154ec3eb7d5fc355da24b4f868a94
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2023-08-14 14:28:00 -0300

    Adicionar as Views

---
+++ SupportClone/SupportClone/Views/ActivitieView.swift
@@ -0,0 +1,20 @@
+//
+//  ActivitiesView.swift
+//  SupportClone
+//
+//  Created by Gabriel Vicentin Negro on 14/08/23.
+//
+
+import SwiftUI
+
+struct ActivitieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ActivitieView_Previews: PreviewProvider {
+    static var previews: some View {
+        ActivitieView()
+    }
+}
---
+++ SupportClone/SupportClone/Views/SupportView.swift
@@ -0,0 +1,20 @@
+//
+//  SupportView.swift
+//  SupportClone
+//
+//  Created by Gabriel Vicentin Negro on 14/08/23.
+//
+
+import SwiftUI
+
+struct SupportView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SupportView_Previews: PreviewProvider {
+    static var previews: some View {
+        SupportView()
+    }
+}
